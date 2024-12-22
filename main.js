6c64984df066a46ba485daef504532e85849f6a9
dolianaleal <dolianalealrivas@gmail.com>
2024-12-22 11:02:30 -0300

    tasks clase 5

---
+++ main.js
@@ -0,0 +1,37 @@
+import { error } from 'console'
+import fs from 'fs'
+
+const ruta = 'ejemplo.txt'
+
+/*
+//fs.writeFileSync('ejemplo.txt','Hello') //Genero un txt nuevo si no existe, sino lo reemplazo
+
+if (fs.existsSync(ruta)) {
+
+    let datos = fs.readFileSync(ruta, 'utf8')
+    console.log(datos);
+
+    fs.appendFileSync(ruta,"Hello \n")
+
+    fs.unlinkSync(ruta)
+} 
+    */
+
+fs.writeFile(ruta, 'Hola', (error) => {
+
+    if (error)
+        console.log("Error al escribir archivo")
+    fs.readFile (ruta,'utf8', (error) => {
+        if (error)
+            console.log("Error en la lectura del archivo")
+        fs.appendFile(ruta, 'Hola \n', (error) => {
+            if (error)
+                console.log("Error en modificar archivo")
+            fs.unlink(ruta, (error) => {
+                if(error)
+                    console.log("Error en la eliminacion de archivo")
+            })
+        })
+    })
+
+})
